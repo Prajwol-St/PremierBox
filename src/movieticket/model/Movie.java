89fdf4819715c043037251bc89229c5b4c22133a
Prajwol <Prajwools272@gmail.com>
2025-06-05 20:32:36 +0545

    pushing frst in main

---
+++ src/movieticket/model/Movie.java
@@ -0,0 +1,24 @@
+package movieticket.model;
+
+public class Movie {
+    private String title;
+    private String genre;
+    private String duration;
+    private String date;
+    private String posterPath;
+
+    public Movie(String title, String genre, String duration, String date) {
+        this.title = title;
+        this.genre = genre;
+        this.duration = duration;
+        this.date = date;
+        this.posterPath = posterPath;
+    }
+
+    public String getTitle() { return title; }
+    public String getGenre() { return genre; }
+    public String getDuration() { return duration; }
+    public String getDate() { return date; }
+    public String getPosterPath() { return posterPath; }
+}
+
