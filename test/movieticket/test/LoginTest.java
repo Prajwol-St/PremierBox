0fad94ae435f4ea3adfaba5ddb457099d8731301
Ishan <ishanjoshi234@gmail.com>
2025-06-13 15:42:19 +0545

    Added LoginTest

--- test/movieticket/test/LoginTest.java
+++ test/movieticket/test/LoginTest.java
@@ -8,6 +8,38 @@ package movieticket.test;
  *
  * @author acer
  */
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
 public class LoginTest {
+    private Login login;
     
+    @Before
+    public void setup(){
+        login = new Login();
+        login.setUser("user1","password123");   
+    }
+    
+    @Test
+    public void testLoginSuccess(){
+        assertTrue(login.login("user1","password123"));
+    }
+    @Test
+    public void testLoginWrongPassword(){
+        assertFalse(login.login("user1","wrongpass"));
+    }
+}
+
+class Login {
+    private String storedUsername = null;
+    private String storedPassword  = null;
+    
+    public void setUser(String username, String password){
+        storedUsername = username;
+        storedPassword = password;
+    }
+    public boolean login(String username, String password){
+        if(storedUsername == null)return false;
+        return storedUsername.equals(username)&& storedPassword.equals(password);
+    }
 }
