6b542b600c13a72aab243c2fb9919f2b9a557cc3
Ishan <ishanjoshi234@gmail.com>
2025-06-20 20:07:17 +0545

    Updated Code

--- src/movieticket/database/MySqlConnection.java
+++ src/movieticket/database/MySqlConnection.java
@@ -15,7 +15,7 @@ public class MySqlConnection implements DbConnection {
     public Connection openConnection() {
         try{
             String username = "root";
-            String password = "xavier@90";
+            String password = "ishanjoshi2006";
             String database = "user_system";
             Class.forName("com.mysql.cj.jdbc.Driver");
             Connection connection;
