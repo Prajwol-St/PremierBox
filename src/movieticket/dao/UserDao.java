496a461ee9ade2106cd66a7f2a34feedbf88767c
Prajwol <Prajwools272@gmail.com>
2025-06-21 13:34:11 +0545

    clean code

--- src/movieticket/dao/UserDao.java
+++ src/movieticket/dao/UserDao.java
@@ -125,8 +125,9 @@ public class UserDao {
         try{
 //           step 3: prepare statement
             PreparedStatement stmnt = conn.prepareStatement(query);
+            String hashedPassword = BCrypt.hashpw(resetReq.getPassword(), BCrypt.gensalt());
 //            step 4: use setstring to prepare query if needed
-            stmnt.setString(1,resetReq.getPassword());
+            stmnt.setString(1,hashedPassword);
             stmnt.setString(2,resetReq.getEmail());
 //            step 5: executequery or executeupdate
             int result = stmnt.executeUpdate();
